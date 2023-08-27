411c8b19efde6c7b5eed7fc988440e057691dcdc
JoseCabrejoVillarCampus <cabre920903@gmail.com>
2023-08-27 12:06:17 -0500

    feat: :sparkles: Cracion de Los Endpoint colecciones
    Creamos los Endpoints para las colecciones

--- app.js
+++ app.js
@@ -7,6 +7,30 @@ dotenv.config();
 let app = express();
 app.use(express.json());
 
+//? Rutas Optencion Del Token
+app.use('/login', appLogin);
+app.use("/user", Routes.appUser);
+
+//? Rutas Para Los Cruds De Todas Las Colecciones
+app.use("/area", Routes.appArea);
+app.use("/categoria", Routes.appCategoria);
+app.use("/diadema", Routes.appDiadema);
+app.use("/email", Routes.appEmail);
+app.use("/equipo", Routes.appEquipo);
+app.use("/estado", Routes.appEstado);
+app.use("/incidencia", Routes.appIncidencia);
+app.use("/mouse", Routes.appMouse);
+app.use("/pantalla", Routes.appPantalla);
+app.use("/reporte", Routes.appReporte);
+app.use("/salonTrainner", Routes.appSalonTrainner);
+app.use("/salon", Routes.appSalon);
+app.use("/teclado", Routes.appTeclado);
+app.use("/telefono", Routes.appTelefono);
+app.use("/tipoIncidencia", Routes.appTipo);
+app.use("/torre", Routes.appTorre);
+app.use("/trainner", Routes.appTrainners);
+
+
 let config = JSON.parse(process.env.MY_SERVER);
 console.log(config);
 app.listen(config, ()=>{
